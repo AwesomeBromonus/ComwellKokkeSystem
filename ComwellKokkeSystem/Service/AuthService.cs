052bb30aca859c1b80def2b9a76df0fb9202df8f
iboops4 <ibrahimabde6@gmail.com>
2025-05-07 19:49:21 +0200

    nn

---
+++ ComwellKokkeSystem/Service/AuthService.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http.Json;
+using Modeller;
+
+public class AuthService : IAuthService
+{
+    private readonly HttpClient _http;
+
+    public AuthService(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task<bool> Login(LoginModel login)
+    {
+        var response = await _http.PostAsJsonAsync("api/users/login", login);
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<bool> Register(RegisterModel user)
+    {
+        var response = await _http.PostAsJsonAsync("api/users/register", user);
+        return response.IsSuccessStatusCode;
+    }
+
+    public Task Logout()
+    {
+        // Kan udvides senere til fx slette localStorage
+        return Task.CompletedTask;
+    }
+}
