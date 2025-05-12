2c60fe3843d13a21ee863e27eed94285282ac562
iboops4 <ibrahimabde6@gmail.com>
2025-05-12 09:07:43 +0200

    u

--- ComwellKokkeSystem/Program.cs
+++ ComwellKokkeSystem/Program.cs
@@ -4,8 +4,7 @@ using ComwellKokkeSystem;
 using Blazored.LocalStorage;
 using Service;
 using ComwellKokkeSystem.Service;
-using MudBlazor;
-using MudBlazor.Services;
+
 
 public class Program
 {
@@ -31,30 +30,7 @@ public class Program
         builder.Services.AddScoped<IAuthService, AuthService>();
         builder.Services.AddScoped<IDelmaalService, DelmaalService>();
 
-        // MudBlazor services
-        builder.Services.AddMudServices();
-
-        // Custom theme
-        var theme = new MudTheme()
-        {
-            Palette = new Palette()
-            {
-                Primary = "#007fa3",          // Comwell-blå
-                Secondary = "#00c2cb",        // Frisk accent
-                Background = "#f3f4f6",       // Lys, rolig baggrund
-                Surface = "#ffffff",          // Kort baggrund
-                AppbarBackground = "#003b4a", // Header
-                TextPrimary = "#111827",
-                DrawerBackground = "#003b4a",
-                DrawerText = "#ffffff"
-            },
-            LayoutProperties = new LayoutProperties()
-            {
-                DefaultBorderRadius = "12px"
-            }
-        };
-
-        builder.Services.AddSingleton<MudTheme>(theme);
+  
 
         await builder.Build().RunAsync();
     }
