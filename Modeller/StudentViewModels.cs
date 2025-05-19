2bc24492ab28b6635cf504e61a5e4edb37cccb1b
Lukas <eaa24luba.students@eaaa.dk>
2025-05-20 01:14:53 +0200

    Add StudentViewModels.cs

--- Modeller/StudentViewModels.cs
+++ Modeller/StudentViewModels.cs
@@ -1,6 +1,25 @@
-﻿namespace Modeller;
+using System;
+using System.Collections.Generic;
 
-public class StudentViewModels
+namespace Modeller
 {
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+        public string Navn { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+        public int DelmaalDone { get; set; }
+        public int DelmaalTotal { get; set; }
+        public int DelmaalPercent => DelmaalTotal > 0 ? (DelmaalDone * 100) / DelmaalTotal : 0;
+        public List<Delmål> DelmaalList { get; set; } = new();
+        public bool IsSelected { get; set; }
+        public int? DaysToSchool { get; set; }
+        public Praktikperiode Praktikperiode { get; set; }
+        public int? ElevplanId { get; set; }
+
+        public List<Praktikperiode> Praktikperioder { get; set; }
+    }
     
+ 
 }
